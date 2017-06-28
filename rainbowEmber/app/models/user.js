b054e37cf89c9047ecf62b615d7415f439349d37
Leland Krych <Leland.Krych@gmail.com>
2017-06-28 15:30:22 -0700

    change favoriteColor keyword so that ember can interpret JSON

--- rainbowEmber/app/models/user.js
+++ rainbowEmber/app/models/user.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  favorite_color: DS.attr('string'),
+  favoritecolor: DS.attr('string'),
   email: DS.attr('string')
 });
