f3bc9568949f201539bbe158e8eedf3af33dee1c
Leland Krych <Leland.Krych@gmail.com>
2017-06-29 00:34:26 -0700

    begin pagination process

--- rainbowEmber/app/routes/index.js
+++ rainbowEmber/app/routes/index.js
@@ -1,7 +1,9 @@
 import Ember from 'ember';
+import InfinityRoute from "ember-infinity/mixins/route";
 
-export default Ember.Route.extend({
-  model(){ //grab information from server and return it
-    return this.store.findAll('user');
+export default Ember.Route.extend(InfinityRoute, {
+  model() {
+    /* Load pages of the User Model, starting from page 1, in groups of 25. */
+    return this.infinityModel("user", { perPage: 5, startingPage: 1 });
   }
 });
