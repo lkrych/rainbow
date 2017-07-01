372e46a40def45fffb236d17d89a5ad56eec9dd4
Leland Krych <Leland.Krych@gmail.com>
2017-07-01 08:29:19 -0700

    remove console log from testdata.js

--- rainbowEmber/app/routes/testdata.js
+++ rainbowEmber/app/routes/testdata.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     Ember.$.getJSON(`?userCount=${params.userCount}`).then((response) => {
-      console.log(response);
       this.refresh();
       this.transitionTo('index');
 
